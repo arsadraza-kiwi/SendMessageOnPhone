7b2651d6d01ad5916b04c7a5a0706e7a41446996
arsadraza-kiwi <arsad.raza@kiwitech.com>
2022-03-22 11:07:58 +0530

    Otp Send

--- src/main/java/com/OtpSendPhone/service/SmsService.java
+++ src/main/java/com/OtpSendPhone/service/SmsService.java
@@ -6,7 +6,6 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import org.springframework.util.MultiValueMap;
-import org.springframework.util.MultiValueMap;
 
 @Component
 public class SmsService {
